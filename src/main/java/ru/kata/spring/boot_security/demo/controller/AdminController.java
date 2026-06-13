7017793bd8fc07fe6ce1e4b663207d9b2be30164
Danil <danil.boew.2003@gmail.com>
2026-06-13 19:27:35 +0300

    done

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,9 +1,7 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.Role;
@@ -63,6 +61,11 @@ public class AdminController {
                 if (role != null) {
                     roles.add(role);
                 }
+            }
+        } else {
+            Role defaultrole = roleService.findByName("ROLE_USER");
+            if (defaultrole != null) {
+                roles.add(defaultrole);
             }
         }
         user.setRoles(roles);
